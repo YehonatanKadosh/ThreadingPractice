602156fd05f4a81421ce97e324f2d711a1f12a7d
YehonatanKadosh <jacojacoj3@gmail.com>
2021-03-17 17:27:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultyThreading
+{
+    class Program
+    {
+        static string Occupied = new string("");
+        static int Counter = 0;
+        static void Main(string[] args)
+        {
+            string LongestName = "Adolph Blaine Charles David Earl Frederick Gerald Hubert Irvin John Kenneth Lloyd Martin Nero Oliver Paul Quincy Randolph Sherman Thomas Uncas Victor William Xerxes Yancy Zeus Wolfeschlegel­steinhausen­bergerdorff­welche­vor­altern­waren­gewissenhaft­schafers­wessen­schafe­waren­wohl­gepflege­und­sorgfaltigkeit­beschutzen­vor­angreifen­durch­ihr­raubgierig­feinde­welche­vor­altern­zwolfhundert­tausend­jahres­voran­die­erscheinen­von­der­erste­erdemensch­der­raumschiff­genacht­mit­tungstein­und­sieben­iridium­elektrisch­motors­gebrauch­licht­als­sein­ursprung­von­kraft­gestart­sein­lange­fahrt­hinzwischen­sternartig­raum­auf­der­suchen­nachbarschaft­der­stern­welche­gehabt­bewohnbar­planeten­kreise­drehen­sich­und­wohin­der­neue­rasse­von­verstandig­menschlichkeit­konnte­fortpflanzen­und­sich­erfreuen­an­lebenslanglich­freude­und­ruhe­mit­nicht­ein­furcht­vor­angreifen­vor­anderer­intelligent­geschopfs­von­hinzwischen­sternartig­raum Sr.";
+            foreach (char item in LongestName)
+            {
+              Task.Run(()=>Stam(item));
+            }
+            Console.ReadLine();
+        }
+
+        public static void Stam(char item)
+        {
+            lock (Occupied)
+            {
+                Console.WriteLine(Counter++ + $" {item}");
+            }
+        }
+
+    }
+}
